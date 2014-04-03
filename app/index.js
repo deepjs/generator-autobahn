b2d67ae89523758986ca47cab585e6fd44093297
Philippe Delabye <phil@chemicalrules.net>
2014-04-03 13:55:02 +0200

    prompts question to fill the package.json template

--- app/index.js
+++ app/index.js
@@ -23,26 +23,50 @@ var AutobahnGenerator = yeoman.generators.Base.extend({
 		// replace it with a short and sweet description of your generator
 		this.log(chalk.magenta('You\'re using the fantastic Autobahn generator.'));
 
-		// var prompts = [{
-		// 	type: 'confirm',
-		// 	name: 'someOption',
-		// 	message: 'Would you like to enable this option?',
-		// 	default: true
-		// }];
+		var prompts = [{
+			name: 'author',
+			message: 'Author name?',
+		},
+		{
+			name: 'email',
+			message: 'Author email?',
+		},
+		{
+			name: 'applicationName',
+			message: 'Name of your application?',
+			default: 'myApp'
+		},
+		{
+			name: 'version',
+			message: 'version number of your application?',
+			default: '0.0.0'
+		},
+		{
+			name: 'description',
+			message: 'Description of your application?'
+		},
+		{
+			name: 'githubURL',
+			message: 'github URL of the repository?'
+		}];
 
-		// this.prompt(prompts, function (props) {
-		// 	this.someOption = props.someOption;
+		this.prompt(prompts, function (props) {
+			this.author = props.author;
+			this.email = props.email;
+			this.applicationName = props.applicationName;
+			this.version = props.version;
+			this.description = props.description;
+			this.githubURL = props.githubURL;
 
-		// 	done();
-		// }.bind(this));
+			done();
+		}.bind(this));
 
-		done();
 	},
 	app: function () {
 		//this.mkdir('www');
 		//this.mkdir('www/templates');
 
-		this.copy('_package.json', 'package.json');
+		this.template('_package.json', 'package.json');
 		this.copy('_bower.json', 'bower.json');
 		this.copy('_index.js', 'index.js');
 	},
