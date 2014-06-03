e9ec700a957059e8ad63879b0cb528a3cc4f97bd
Philippe Delabye <phil@chemicalrules.net>
2014-06-03 14:36:56 +0200

    now we can exclude repo for the grunt gitify command

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -5,7 +5,8 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
 		gitify: {
-			prefixes: ["deep", "autobahn"]
+			prefixes: ["deep", "autobahn"],
+			exclude: ["deep-shell"]
 		}
 	});
 
@@ -18,14 +19,17 @@ module.exports = function(grunt) {
 		grunt.log.writeln('Currently running Gitify task.');
 		var packages = grunt.config('pkg').dependencies;
 		var prefixes = grunt.config('gitify').prefixes;
+		var exclude = grunt.config('gitify').exclude;
 		var regex = new RegExp("^((" + prefixes.join(')|(') + "))");
+		var regexExclude = new RegExp("^((" + exclude.join(')|(') + "))");
+
 		var promises = [];
 		for (var key in packages) {
 			var pack = packages[key].split("#");
 			var packURI = pack[0].substring(4);
 			var packVersion = pack[1] || "master";
 
-			if (regex.test(key))
+			if (!regexExclude.test(key) && regex.test(key))
 			{
 				console.log(key, ":", packURI);
 				var d = deep.shell()
