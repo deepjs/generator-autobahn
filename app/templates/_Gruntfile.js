7e37c3156091141daf5186b9c7ffd5da9c3ba852
Gilles Coomans <gilles.coomans@gmail.com>
2014-06-03 15:28:47 +0200

    work on gitify bower/node

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -3,8 +3,8 @@ module.exports = function(grunt) {
 	require('deep-shell');
 
 	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
 		gitify: {
+			namespace:"node",
 			prefixes: ["deep", "autobahn"],
 			exclude: ["deep-shell"]
 		}
@@ -16,33 +16,43 @@ module.exports = function(grunt) {
 
 	grunt.registerTask('gitify', 'Making git repository of deep* and autobahn modules.', function() {
 		var done = this.async();
-		grunt.log.writeln('Currently running Gitify task.');
-		var packages = grunt.config('pkg').dependencies;
-		var prefixes = grunt.config('gitify').prefixes;
-		var exclude = grunt.config('gitify').exclude;
+		grunt.log.writeln('Grunt Gitify task start.');
+
+		var config = grunt.config('gitify'),
+			packageFile, 
+			folder;
+
+		if(config.namespace == 'bower')
+		{
+			packageFile = "bower.json";
+			folder = "bower_components"
+		}
+		else
+		{
+			packageFile = "package.json";
+			folder = "node_modules"
+		}
+		var packages = grunt.file.readJSON(packageFile);
+		var prefixes = config.prefixes;
+		var exclude = config.exclude;
+
 		var regex = new RegExp("^((" + prefixes.join(')|(') + "))");
 		var regexExclude = new RegExp("^((" + exclude.join(')|(') + "))");
-
 		var promises = [];
 		for (var key in packages) {
 			var pack = packages[key].split("#");
 			var packURI = pack[0].substring(4);
 			var packVersion = pack[1] || "master";
-
 			if (!regexExclude.test(key) && regex.test(key))
 			{
 				console.log(key, ":", packURI);
 				var d = deep.shell()
-				.exec("rm -rf ./node_modules/" + key)
-				.cd("./node_modules")
+				.exec("rm -rf ./"+folder+"/" + key)
+				.cd("./"+folder)
 				//.log("git clone " + packURI + " " + key)
 				.exec("git clone " + packURI + " " + key)
 				//.log("git cloned " + packURI)
-				//.pwd()
-				//.ls()
 				.cd("./" + key)
-				//.log()
-				//.ls()
 				.exec("git checkout " + packVersion).log();
 				promises.push(d);
 			}
@@ -53,7 +63,7 @@ module.exports = function(grunt) {
 			done(true);
 		}).fail(function (argument) {
 			done(false);
-		})
+		});
 
 	});
 
